524db12771a863a614c69f4c1c5f305aeafa6944
Tobbiloba <salauoluwatobiloba0@gmail.com>
2022-11-25 12:55:59 +0100

    linked localStorage to box

--- assets/js/app.js
+++ assets/js/app.js
@@ -16,6 +16,34 @@ closeIcon.addEventListener("click", () => {
     popupBox.classList.remove("show")
 })
 
+
+function showNotes() {
+    notes.forEach((note) => {
+        let liTag = `<li class="note">
+        <div class="details">
+          <p>This is the Title</p>
+          <span>
+            lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean
+            commodo ligula eget dolor. Aenean massa. Cum sociis natoque
+            penatibus et magnis dis parturient montes
+          </span>
+        </div>
+        <div class="bottom-content">
+          <span>April 3, 2002</span>
+          <div class="settings">
+            <i class="ri-more-line"></i>
+            <ul class="menu">
+              <li>
+                <i class="ri-pencil-line icon1">Edit</i>
+                <i class="ri-delete-bin-6-line icon2">Delete</i>
+              </li>
+            </ul>
+          </div>
+        </div>`;
+        addBox.insertAdjacentHTML("afterend", liTag)
+    })
+}
+showNotes();
 addBtn .addEventListener('click', e => {
     e.preventDefault();
     let noteTitle = titleTag.value;
